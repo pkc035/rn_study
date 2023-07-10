823386c5ab896eeb6f6f1c391a40daba7e4a66a6
pkc035 <pkc0305@gmail.com>
2023-07-10 18:44:43 +0900

    feat : flex box

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hellow world!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!"/>
+        <Button title="Add Goal"/>
+      </View>
+      <View style={styles.goalContainer}>
+        <Text>List of Goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer:{
+    flex:1,
+    padding:50,
+    paddingHorizontal: 16
   },
+  inputContainer:{
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor:'#cccccc',
+    width:'70%',
+    margin:8,
+    padding:8
+  },
+  goalContainer:{
+    flex:4,
+  }
 });
