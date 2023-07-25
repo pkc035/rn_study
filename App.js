64be3b2d52d4d2270e99389a053049aa12e7ddbc
pkc035 <pkc0305@gmail.com>
2023-07-25 17:25:04 +0900

    feat : flatlist

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
+import {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView,FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredGoalText) {
+    setEnteredGoalText(enteredGoalText);
+  }
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals, 
+      {text : enteredGoalText, id : Math.random().toString()}
+    ]); 
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!"/>
-        <Button title="Add Goal"/>
+        <TextInput style={styles.textInput} placeholder="Your course goal!"
+        onChangeText={goalInputHandler}/>
+        <Button title="Add Goal" onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalContainer}>
-        <Text>List of Goals...</Text>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData)=>{
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}
+          />
       </View>
     </View>
   );
@@ -39,5 +66,15 @@ const styles = StyleSheet.create({
   },
   goalContainer:{
     flex:4,
+  },
+  goalItem:{
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc'    
+  },
+  goalText:{
+    color: 'white'
   }
+
 });
